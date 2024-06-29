5bf85f32951dbfcda7de57962ec23008dc60a120
behnam <behnam.rahmani8@gmail.com>
2024-06-29 11:42:12 +0330

    fix build bug

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="container flex flex-col-reverse md:flex-row items-center justify-between pb-28">
             <div className='text-center md:text-start'>
                 <h1 className='font-bold text-5xl lg:text-7xl py-0.5'> more than just shorter links</h1>
-                <p className='font-bold text-base lg:text-2xl text-gray-400 py-1'>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+                <p className='font-bold text-base lg:text-2xl text-gray-400 py-1'>{`Build your brand's recognition and get detailed insights on how your links are performing.`}</p>
                 <Button variant="default" className='rounded-full my-4'>Get Started</Button>
             </div>
             <div className='relative w-full md:w-1/2 aspect-[4/3] shrink-0'>
